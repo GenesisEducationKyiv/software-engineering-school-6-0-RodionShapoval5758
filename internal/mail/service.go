0f25e7aa326fea58f968ae50cd37abb9ea87bcc4
Rodion Shapoval <shapovalrodion5758@gmail.com>
2026-05-17 00:37:24 +0300

    refactor: remove the exported interface to comply with the DIP and use black-box in tests

--- internal/mail/service.go
+++
@@ -1,8 +0,0 @@
-package mail
-
-import "GithubReleaseNotificationAPI/internal/domain"
-
-type Service interface {
-	SendConfirmationEmail(toEmail, repoName, confirmToken string) error
-	SendReleaseNotification(toEmail string, unsubscribeToken string, release *domain.Release) error
-}
