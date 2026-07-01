b396fe5cef6c79ac740e5d04adeb82eb7a18b747
Rodion Shapoval <shapovalrodion5758@gmail.com>
2026-07-01 17:27:29 +0300

    feat(monitoring): add GRPC TLS cert config fields

--- services/monitoring/internal/config/config.go
+++ services/monitoring/internal/config/config.go
@@ -13,6 +13,9 @@ var (
 	ErrMissingDatabaseURL          = errors.New("DATABASE_URL is required")
 	ErrMissingNATSUrl              = errors.New("NATS_URL is required")
 	ErrMissingSubscriptionGRPCAddr = errors.New("SUBSCRIPTION_GRPC_ADDR is required")
+	ErrMissingGRPCTLSCACert        = errors.New("GRPC_TLS_CA_CERT is required")
+	ErrMissingGRPCTLSCert          = errors.New("GRPC_TLS_CERT is required")
+	ErrMissingGRPCTLSKey           = errors.New("GRPC_TLS_KEY is required")
 )
 
 type Config struct {
@@ -21,6 +24,9 @@ type Config struct {
 	NATSUrl              string
 	ScanInterval         time.Duration
 	SubscriptionGRPCAddr string
+	GRPCTLSCACert        string
+	GRPCTLSCert          string
+	GRPCTLSKey           string
 }
 
 func Load() (*Config, error) {
@@ -42,6 +48,9 @@ func loadFromEnv() *Config {
 		NATSUrl:              os.Getenv("NATS_URL"),
 		ScanInterval:         interval,
 		SubscriptionGRPCAddr: os.Getenv("SUBSCRIPTION_GRPC_ADDR"),
+		GRPCTLSCACert:        os.Getenv("GRPC_TLS_CA_CERT"),
+		GRPCTLSCert:          os.Getenv("GRPC_TLS_CERT"),
+		GRPCTLSKey:           os.Getenv("GRPC_TLS_KEY"),
 	}
 }
 
@@ -60,6 +69,15 @@ func (c *Config) validate() error {
 	}
 	if c.SubscriptionGRPCAddr == "" {
 		return ErrMissingSubscriptionGRPCAddr
+	}
+	if c.GRPCTLSCACert == "" {
+		return ErrMissingGRPCTLSCACert
+	}
+	if c.GRPCTLSCert == "" {
+		return ErrMissingGRPCTLSCert
+	}
+	if c.GRPCTLSKey == "" {
+		return ErrMissingGRPCTLSKey
 	}
 
 	return nil
